4691d44fdc9f811e8beda94a72c42211b6b65a6a
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-16 21:29:50 +0200

    test pysqlite3 settings

--- crowdfunding_wep_app/crowdfunding_wep_app/settings.py
+++ crowdfunding_wep_app/crowdfunding_wep_app/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
+import sys
+
+__import__('pysqlite3')
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,6 +80,7 @@ WSGI_APPLICATION = 'crowdfunding_wep_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
