623c632f31ce33da01211c8fb4911497ab3dd7f1
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-16 15:13:43 +0200

    added initial user model

--- crowdfunding_wep_app/login_registration/models.py
+++ crowdfunding_wep_app/login_registration/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=45)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    email = models.EmailField(max_length=45)
+    password = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45)
+    profile_pic = models.CharField(max_length=45)
+    is_activated = models.BooleanField(default=False)
+    birth_date = models.DateField(null=True, blank=True)
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
+    facebook_profile_id = models.CharField(max_length=45)
