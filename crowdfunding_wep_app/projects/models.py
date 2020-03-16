e5ae6eae670dafa109bdad2d7bd809d8406b4356
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-16 18:07:25 +0200

    added initial project model

--- crowdfunding_wep_app/projects/models.py
+++ crowdfunding_wep_app/projects/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from login_registration.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=45)
+
+
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=45)
+    description = models.TextField
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    total_target = models.DecimalField
+    start_date = models.DateField
+    end_date = models.DateField
+    tags = models.ManyToManyField(Tags, blank=True, verbose_name="List of tags", related_name="number_of_uses")
