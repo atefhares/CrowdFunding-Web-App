e6254ca65b6046aefd50399c4c43717c371301cd
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-03-24 00:44:27 +0200

    added django-cleanup app to cleanup no longer needed files and requirements file

--- crowdfunding_web_app/crowdfunding_web_app/settings.py
+++ crowdfunding_web_app/crowdfunding_web_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'login_registration',
     'user_profile',
     'projects',
+    'django_cleanup',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
