e02c6e523c67791c4cc644998570df2a891dda36
mohamedsalama11022 <mohamed.helmy11022@gmail.com>
2020-03-31 01:51:52 +0200

    editing the user profile model to be suitable for the email authentication

--- crowdfunding_web_app/accounts/models.py
+++ crowdfunding_web_app/accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django_countries.fields import CountryField
 
 from django.contrib.auth.models import User
 # Create your models here.
+
 
 class Country(models.Model):
     name = models.CharField(max_length=45)
@@ -9,13 +11,22 @@ class Country(models.Model):
     def __str__(self):
         return self.name
 
+
 class UserProfile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=45,default=None,null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    phone_number = models.CharField(max_length=45, default=None, null=True)
     profile_pic = models.ImageField(null=True)
     birth_date = models.DateField(null=True, blank=True)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True)
-    facebook_profile_id = models.CharField(null=True, blank=True, max_length=45)
+    country = CountryField(null=True)
+    facebook_profile_id = models.CharField(
+        null=True, blank=True, max_length=45)
+    time_stamp = models.DateTimeField(null=True)
+    expires = models.DateTimeField(null=True)
+    is_active = models.BooleanField(default=False)
+    key = models.CharField(null=False, blank=False,
+                           max_length=45, default=None)
+    once_activation = models.BooleanField(default=False)
+
     def __str__(self):
         return self.user.email
-
