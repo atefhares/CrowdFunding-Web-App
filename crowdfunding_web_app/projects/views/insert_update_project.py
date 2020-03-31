b089aa04e9ceb20bf0613f5d0766a11a46b07c62
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-03-31 17:04:56 +0200

    fixing bugs in create project

--- crowdfunding_web_app/projects/views/insert_update_project.py
+++ crowdfunding_web_app/projects/views/insert_update_project.py
@@ -1,9 +1,7 @@
-from django.conf import settings
-# from datetime import datetime
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_protect
 
-from projects.models import Project
+from projects.models import Project, Category
+from login_registration.models import Country
 
 
 def handle_update_project_request(request):
@@ -13,5 +11,14 @@ def handle_update_project_request(request):
 def handle_create_new_project_request(request):
     print(request)
     print(request.POST)
-    print(request.POST.get("category"))
-    return render(request, "projects/create_project.html")
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        all_countries = Country.objects.all()
+
+        render_data = {
+            "categories": all_categories,
+            "countries": all_countries
+        }
+        return render(request, "projects/create_project.html", render_data)
+    else:
+        None
