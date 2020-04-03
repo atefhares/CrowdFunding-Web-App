e21d334d3201fbecb5075b73fa6dade06a060d8f
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-04-03 22:54:51 +0200

    basic layout for project details page and fix of font awesome lib

--- crowdfunding_web_app/projects/views/view_project.py
+++ crowdfunding_web_app/projects/views/view_project.py
@@ -6,4 +6,7 @@ def handle_view_project_request(request, project_id):
     if settings.DEBUG:
         print("request: ", request)
         print("project_id: ", project_id)
-    return render(request, "projects/project_details.html")
+
+    context = {}
+
+    return render(request, "projects/project_details.html", context)
