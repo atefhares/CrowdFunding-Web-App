aeaa34411d353396ec16570e345dc1d357abba1a
Tharwat Abou-Helal <tharwat96@gmail.com>
2020-04-03 23:14:24 +0200

    basic project data are now shown (styling is still messed up)

--- crowdfunding_web_app/projects/views/view_project.py
+++ crowdfunding_web_app/projects/views/view_project.py
@@ -1,12 +1,19 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 
+from projects.models import Project
+
 
 def handle_view_project_request(request, project_id):
     if settings.DEBUG:
         print("request: ", request)
         print("project_id: ", project_id)
 
-    context = {}
-
+    context = {'project': get_project(project_id)}
+    print(context)
     return render(request, "projects/project_details.html", context)
+
+
+def get_project(id):
+    project = Project.objects.get(id=id)
+    return project
