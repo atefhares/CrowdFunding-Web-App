c4c8c2a96344647bc0e9c484a7b3a45d42239c02
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-04-04 15:48:16 +0200

    fixing bugs in list projects, create new project, adding logic of update project [not tested]

--- crowdfunding_web_app/projects/urls.py
+++ crowdfunding_web_app/projects/urls.py
@@ -4,6 +4,6 @@ from projects.views import view_project, list_projects, insert_update_project
 urlpatterns = [
     path('', list_projects.handle_list_all_projects_request, name='list_projects'),
     path('create', insert_update_project.handle_create_new_project_request),
-    path('update', insert_update_project.handle_update_project_request),
+    path('update/<project_id>', insert_update_project.handle_update_project_request),
     path('<project_id>', view_project.handle_view_project_request),
 ]
